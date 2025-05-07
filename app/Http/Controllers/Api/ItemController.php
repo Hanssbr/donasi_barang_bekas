f23c38816c84baf4e7d1fe8b1bedaf09e0f33d2d
Hanssbr <rayhaniqbalbr@gmail.com>
2025-05-08 05:25:23 +0700

    add photo

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -42,6 +42,7 @@ class ItemController extends Controller
             $photo->storeAs('public/photo', $photo->hashName());
             $item->photo = $photo->hashName();
         }
+        dd($request->file('photo'));
 
         $item->save();
         $item = Item::with('user')->find($item->id);
